387817619da2b92525560eecdd96725077605c45
dolansgists <dolan@sports-it.com>
2017-06-08 09:06:57 -0700

    Fix for-in loop
    Check if key exists and is not a function in for-in loop before using that key.

--- dist/moment-round.js
+++ dist/moment-round.js
@@ -22,15 +22,17 @@
     var subRatio = 1;
     var maxValue ;
     for (var i in keys) {
-      var k = keys[i];
-      if (k === key) {
-        value = this._d['get' + key]();
-        maxValue = maxValues[i];
-        rounded = true;
-      } else if(rounded) {
-        subRatio *= maxValues[i];
-        value += this._d['get' + k]() / subRatio;
-        this._d['set' + k](0);
+      if(keys.hasOwnProperty(i)) {
+        var k = keys[i];
+        if (k === key) {
+          value = this._d['get' + key]();
+          maxValue = maxValues[i];
+          rounded = true;
+        } else if(rounded) {
+          subRatio *= maxValues[i];
+          value += this._d['get' + k]() / subRatio;
+          this._d['set' + k](0);
+        }
       }
     };
 
